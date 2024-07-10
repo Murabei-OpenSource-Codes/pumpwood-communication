b17f08e749aec3e5c8a832c2772d8e63ee1ccca3
Andrpe Baceti <a.baceti@murabei.com>
2024-07-10 11:26:23 -0300

    Update docs

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
 rm -R build/
 python3 setup.py build sdist bdist_wheel
 
-pdocs as_html --overwrite pumpwood_communication --output_dir site
-pdocs as_markdown --overwrite pumpwood_communication --output_dir docs
+pdocs as_html --overwrite pumpwood_communication --output_dir docs
+pdocs as_markdown --overwrite pumpwood_communication --outut_dir docs
 
 git add --all
 git commit -m "Building a new version ${VERSION}"
