93c94d06390b5da667eca78c35ded5d54de3e986
Andrpe Baceti <a.baceti@murabei.com>
2024-07-10 10:33:11 -0300

    Update documentation

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
 rm -R build/
 python3 setup.py build sdist bdist_wheel
 
+pdocs as_html --overwrite pumpwood_communication --output_dir site
+pdocs as_markdown --overwrite pumpwood_communication --output_dir docs
+
 git add --all
 git commit -m "Building a new version ${VERSION}"
 git tag -a ${VERSION} -m "Building a new version ${VERSION}"
---
+++ docs/pumpwood_communication/index.md
@@ -0,0 +1,10 @@
+# Module pumpwood_communication
+
+## Sub-modules
+
+* [pumpwood_communication.airflow](airflow/)
+* [pumpwood_communication.exceptions](exceptions/)
+* [pumpwood_communication.hash](hash/)
+* [pumpwood_communication.microservices](microservices/)
+* [pumpwood_communication.misc](misc/)
+* [pumpwood_communication.serializers](serializers/)
