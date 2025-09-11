f15c698288976a683a6f9a4ee82bfed2d6c6fa47
André Andrade Baceti <a.baceti@murabei.com>
2025-09-11 18:00:20 -0300

    Building a new version 2.2.22

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
 rm -R build/
 python3 setup.py build sdist bdist_wheel
 
-pdoc --docformat="google" src/pumpwood_communication -o ./docs
+# pdoc --docformat="google" src/pumpwood_communication -o ./docs
 
 git add --all
 git commit -m "Building a new version ${VERSION}"
