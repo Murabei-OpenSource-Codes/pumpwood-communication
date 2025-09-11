a26b6685e22b50a720030f6c1f0e04cbbcb9cce6
André Andrade Baceti <a.baceti@murabei.com>
2025-09-11 17:58:36 -0300

    Building a new version 2.2.21

--- build/lib/pumpwood_communication/microservice_abc/simple/batch.py
+++ build/lib/pumpwood_communication/microservice_abc/simple/batch.py
@@ -1,9 +1,10 @@
 """Module for Simple Batch end-points of microservice."""
 import pandas as pd
 from abc import ABC
-from typing import List
+from typing import List, Union, Dict
 from pumpwood_communication.microservice_abc.base import (
     PumpWoodMicroServiceBase)
+from pumpwood_communication.serializers import CompositePkBase64Converter
 
 
 class ABCSimpleBatchMicroservice(ABC, PumpWoodMicroServiceBase):
@@ -50,3 +51,106 @@ class ABCSimpleBatchMicroservice(ABC, PumpWoodMicroServiceBase):
             'limit': limit}
         return self.request_post(
             url=url_str, data=data, auth_header=auth_header)
+
+    @staticmethod
+    def _build_pivot_url(model_class):
+        return "rest/%s/pivot/" % (model_class.lower(), )
+
+    def pivot(self, model_class: str, columns: List[str] = [],
+              format: str = 'list', filter_dict: dict = {},
+              exclude_dict: dict = {}, order_by: List[str] = [],
+              variables: List[str] = None, show_deleted: bool = False,
+              add_pk_column: bool = False, auth_header: dict = None,
+              as_dataframe: bool = False
+              ) -> Union[List[dict], Dict[str, list], pd.DataFrame]:
+        """Pivot object data acording to columns specified.
+
+        Pivoting per-se is not usually used, beeing the name of the function
+        a legacy. Normality data transformation is done at the client level.
+
+        Args:
+            model_class (str):
+                Model class to check search parameters.
+            columns (List[str]):
+                List of fields to be used as columns when pivoting the data.
+            format (str):
+                Format to be used to convert pandas.DataFrame to
+                dictionary, must be in ['dict','list','series',
+                'split', 'records','index'].
+            filter_dict (dict):
+                Same as list function.
+            exclude_dict (dict):
+                Same as list function.
+            order_by (List[str]):
+                 Same as list function.
+            variables (List[str]):
+                List of the fields to be returned, if None, the default
+                variables will be returned. Same as fields on list functions.
+            show_deleted (bool):
+                Fields with deleted column will have objects with deleted=True
+                omited from results. show_deleted=True will return this
+                information.
+            add_pk_column (bool):
+                If add pk values of the objects at pivot results. Adding
+                pk key on pivot end-points won't be possible to pivot since
+                pk is unique for each entry.
+            auth_header (dict):
+                Auth header to substitute the microservice original
+                at the request (user impersonation).
+            as_dataframe (bool):
+                If results should be returned as a dataframe.
+
+        Returns:
+            Return a list or a dictinary depending on the format set on
+            format parameter.
+
+        Raises:
+            PumpWoodException:
+                'Columns must be a list of elements.'. Indicates that the list
+                argument was not a list.
+            PumpWoodException:
+                'Column chosen as pivot is not at model variables'. Indicates
+                that columns that were set to pivot are not present on model
+                variables.
+            PumpWoodException:
+                "Format must be in ['dict','list','series','split',
+                'records','index']". Indicates that format set as paramenter
+                is not implemented.
+            PumpWoodException:
+                "Can not add pk column and pivot information". If
+                add_pk_column is True (results will have the pk column), it is
+                not possible to pivot the information (pk is an unique value
+                for each object, there is no reason to pivot it).
+            PumpWoodException:
+                "'value' column not at melted data, it is not possible
+                to pivot dataframe.". Indicates that data does not have a value
+                column, it must have it to populate pivoted table.
+        """
+        url_str = self._build_pivot_url(model_class)
+        post_data = {
+            'columns': columns, 'format': format,
+            'filter_dict': filter_dict, 'exclude_dict': exclude_dict,
+            'order_by': order_by, "variables": variables,
+            "show_deleted": show_deleted, "add_pk_column": add_pk_column}
+        pivot_results = self.request_post(
+            url=url_str, data=post_data, auth_header=auth_header)
+
+        if not add_pk_column:
+            if as_dataframe:
+                return pd.DataFrame(pivot_results)
+            else:
+                return pivot_results
+        else:
+            pd_pivot_results = pd.DataFrame(pivot_results)
+            if len(pd_pivot_results) != 0:
+                fill_options = self.fill_options(
+                    model_class=model_class, auth_header=auth_header)
+                primary_keys = fill_options["pk"]["column"]
+                print('fill_options["pk"]["column"]:', fill_options["pk"]["column"])
+                pd_pivot_results["pk"] = pd_pivot_results[primary_keys].apply(
+                    CompositePkBase64Converter.dump,
+                    primary_keys=primary_keys, axis=1)
+            if as_dataframe:
+                return pd_pivot_results
+            else:
+                return pd_pivot_results.to_dict(format)
