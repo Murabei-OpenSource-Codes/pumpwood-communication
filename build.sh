7bc8844a13fc2a2b2bae008b76c52e3c36577cd6
André Andrade Baceti <a.baceti@murabei.com>
2026-06-19 10:48:00 -0300

    Building a new version 2.4.38-b.0

--- build.sh
+++ build.sh
@@ -17,10 +17,7 @@ fi
 sed -e 's#{VERSION}#'"${VERSION}"'#g' pyproject_template.toml > pyproject.toml
 
 rm -R build/
-
 poetry build
-
-# pdoc --docformat="google" src/pumpwood_communication -o ./docs
 
 git add --all
 git commit -m "Building a new version ${VERSION}"
